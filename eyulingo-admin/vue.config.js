b3affc73276a963ffbf8e7a2192c061ad928e582
ddyasmile <1271569621@qq.com>
2019-07-04 17:37:46 +0800

    [add] frontend login

---
+++ eyulingo-admin/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    //axios域代理，解决axios跨域问题
+    publicPath: '/',
+    devServer: {
+        proxy: {
+            '': {
+                target: 'http://47.103.15.32:8080/',
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+
+                }
+            }
+        }
+    }
+}
